2d91986b8db231b2942532838f6fa536a4eb38ca
murphyqwek <113290803+murphyqwek@users.noreply.github.com>
2024-09-17 02:22:11 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,75 @@
+import java.util.Random;
+import java.lang.Math;
+
+public class Main {
+    private static float Calculate1(float x){
+        float temp = (float) Math.pow(x/2, x);
+        temp = (float) Math.pow((Math.exp(x) + 1) / 2, temp);
+        temp = (float) Math.exp(temp);
+
+        return temp;
+    }
+
+    private static float Calculate2(float x){
+        float temp = (float)((Math.sqrt(Math.abs(x)) + 1) / 5);
+        temp = (float) Math.pow(temp, 2);
+        temp = (float) Math.log(temp);
+
+        return temp;
+    }
+
+    private static float Calculate3(float x){
+        float temp = (float) ( ((Math.log(Math.abs(x))) - 0.5 ) / Math.cos(x));
+
+        temp = (float) Math.pow(temp, 2);
+        temp = (float) Math.pow(temp, 3);
+
+        temp = temp / 4;
+
+        float temp1 = (float) Math.sin(Math.atan(1 / Math.exp(Math.abs(x))));
+
+        temp = (float) Math.pow(temp, temp1);
+
+        return temp;
+    }
+
+    private static float[][] GenerateZArray(short[]z, float[] x){
+        float[][] z1 = new float[7][18];
+
+        for(int i = 0; i < 7; i++){
+            for(int j = 0; j < 18; j++){
+                if(z[i] == 16)
+                    z1[i][j] = Calculate1(x[j]);
+                else if (z[i] == 8 || z[i] == 12 || z[i] == 18)
+                    z1[i][j] = Calculate2(x[j]);
+                else
+                    z1[i][j] = Calculate3(x[j]);
+            }
+        }
+
+        return z1;
+    }
+
+    private static void PrintMatrix(float[][] z1){
+        for(float[] row : z1){
+            for(float number: row){
+                System.out.printf("%10.3f ", number);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        short[] z = {18, 16, 14, 12, 10, 8, 6};
+
+        //Generate massive x
+        float[] x = new float[18];
+        for(int i = 0; i < 18; i++)
+            x[i] = rnd.nextFloat(-8.0f, 10.0f + Float.MIN_VALUE);
+
+        float[][] z1 = GenerateZArray(z, x);
+
+        PrintMatrix(z1);
+    }
+}
