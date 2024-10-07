91ba8044320e0d8c09a6d28f8c089dd2f11a0086
murphyqwek <113290803+murphyqwek@users.noreply.github.com>
2024-10-07 22:32:24 +0700

    Add files via upload

---
+++ Lab2/src/Main.java
@@ -0,0 +1,24 @@
+import pokemon.*;
+import ru.ifmo.se.pokemon.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Battle b = new Battle();
+        Pokemon p1A = new Celebi("Дурень", 1);
+        Pokemon p2A = new Eevee("Квадробер", 1);
+        Pokemon p3A = new Flygon("Пикми", 1);
+        b.addAlly(p1A);
+        b.addAlly(p2A);
+        b.addAlly(p3A);
+
+        Pokemon p1F = new Trapinch("Сергей Симонов", 1);
+        Pokemon p2F = new Vibrava("Кирпич", 1);
+        Pokemon p3F = new Flygon("Maxwell", 1);
+        b.addFoe(p1F);
+        b.addFoe(p2F);
+        b.addFoe(p3F);
+
+
+        b.go();
+    }
+}
